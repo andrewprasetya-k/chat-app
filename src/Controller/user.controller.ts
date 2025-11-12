49208e5526c44331cba26f700366d124695b6129
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-11-12 16:46:49 +0700

    added user service and controller

--- src/Controller/user.controller.ts
+++ src/Controller/user.controller.ts
@@ -1,24 +1,26 @@
 import { Controller, Get, Put, UseGuards, Request, Body } from '@nestjs/common';
 import { AuthGuard } from '../Auth/auth.guard';
+import { UserService } from 'src/Service/user.service';
 
 @Controller('users')
 export class UserController {
+    constructor(private readonly UserService: UserService) {}
   
-  // Route tidak dilindungi (public)
-  @Get('public')
-  getPublicData() {
-    return { message: 'This is public data' };
-  }
-  
-  // Route dilindungi (perlu JWT)
-  @UseGuards(AuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
+    // Route tidak dilindungi (public)
+    @Get('public')
+    getPublicData() {
+    return this.UserService.getUser();
+    }
+
+    // Route dilindungi (perlu JWT)
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
     // Bisa akses user data dari JWT payload
     return {
-      userId: req.user.sub,
-      email: req.user.email,
-      username: req.user.username,
+        userId: req.user.sub,
+        email: req.user.email,
+        username: req.user.username,
     };
-  }
+    }
 }
