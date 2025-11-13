44b3c99aff63e89ec360324375be8c73f4826dff
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-11-13 16:44:38 +0700

    prettier

--- src/Chat/chat.service.ts
+++ src/Chat/chat.service.ts
@@ -5,40 +5,39 @@ import { CreateRoomDto } from './Dto/create-room.dto';
 
 @Injectable()
 export class ChatService {
-  constructor(private readonly supabase: SupabaseService) {}
+  constructor(private readonly supabase: SupabaseService) { }
 
   // Dummy untuk test
   getAllMessages() {
     return [{ id: 1, text: 'Hello, world!' }];
   }
 
-  async getMessagesByRoom(room_id:string){
+  async getMessagesByRoom(room_id: string) {
     try {
-        const client = this.supabase.getClient();
-        const { data, error } = await client
-          .from('chat_message')
-          .select(`
+      const client = this.supabase.getClient();
+      const { data, error } = await client
+        .from('chat_message')
+        .select(
+          `
             message_text,
             created_at,
             user:cm_usr_id (
               usr_nama_lengkap
             )
-          `)
-          .eq('cm_cr_id', room_id)
-          .order('created_at', { ascending: true });
+          `,
+        )
+        .eq('cm_cr_id', room_id)
+        .order('created_at', { ascending: true });
 
-        if (error) {
-          throw new InternalServerErrorException(error.message);
-        }
-        return data;
-    } catch (error) {
-        
-    }
+      if (error) {
+        throw new InternalServerErrorException(error.message);
+      }
+      return data;
+    } catch (error) { }
   }
 
   // Kirim pesan ke Supabase
-  async sendMessage(dto: SendMessageDto) 
-  {
+  async sendMessage(dto: SendMessageDto) {
     const { cm_cr_id, cm_usr_id, message_text } = dto;
 
     try {
@@ -63,40 +62,43 @@ export class ChatService {
         data: data[0],
       };
     } catch (error: any) {
-      throw new InternalServerErrorException(error?.message || 'Failed to send message');
+      throw new InternalServerErrorException(
+        error?.message || 'Failed to send message',
+      );
     }
   }
 
-    async createRoom(dto: CreateRoomDto) {
+  async createRoom(dto: CreateRoomDto) {
     const client = this.supabase.getClient();
     const { cr_name, cr_is_group, members } = dto;
 
     try {
-    // Buat room baru
-    const { data: room, error: roomError } = await client
+      // Buat room baru
+      const { data: room, error: roomError } = await client
         .from('chat_room')
         .insert([{ cr_name, cr_is_group }])
         .select()
         .single();
 
-    if (roomError) throw roomError;
+      if (roomError) throw roomError;
 
-    // Tambahkan anggota ke chat_room_members
-    const membersToInsert = members.map((usr_id) => ({
+      // Tambahkan anggota ke chat_room_members
+      const membersToInsert = members.map((usr_id) => ({
         crm_cr_id: room.cr_id,
         crm_usr_id: usr_id,
-    }));
+      }));
 
-    const { error: memberError } = await client
+      const { error: memberError } = await client
         .from('chat_room_member')
         .insert(membersToInsert);
 
-    if (memberError) throw memberError;
+      if (memberError) throw memberError;
 
-    return { success: true, room };
+      return { success: true, room };
     } catch (error: any) {
-    throw new InternalServerErrorException(error.message || 'Failed to create room');
+      throw new InternalServerErrorException(
+        error.message || 'Failed to create room',
+      );
     }
-    }
-
+  }
 }
