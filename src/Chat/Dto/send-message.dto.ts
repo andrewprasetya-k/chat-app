10de3d7b1f24d8b61403542c2819fee2f3137eb8
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-12-03 11:18:21 +0700

    remove suer id frmo to )auto read from controller_

--- src/Chat/Dto/send-message.dto.ts
+++ src/Chat/Dto/send-message.dto.ts
@@ -5,10 +5,6 @@ export class SendMessageDto {
   @IsNotEmpty()
   cm_cr_id: string; // ID room tempat pesan dikirim
 
-  @IsUUID()
-  @IsNotEmpty()
-  cm_usr_id: string; // ID user pengirim pesan
-
   @IsString()
   @IsNotEmpty()
   message_text: string; // Isi pesan
