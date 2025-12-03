4fdd40eefbdd236d3be9255a96d73c439ca8ddec
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-12-03 11:19:05 +0700

    added authmnodeoue

--- src/Chat/chat.module.ts
+++ src/Chat/chat.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ChatController } from './Controller/chat.controller';
 import { ChatService } from './Service/chat.service';
 import { SupabaseModule } from 'src/Supabase/supabase.module';
+import { AuthModule } from 'src/Auth/auth.module';
 
 @Module({
-  imports: [SupabaseModule],
+  imports: [SupabaseModule, AuthModule],
   controllers: [ChatController],
   providers: [ChatService],
 })
