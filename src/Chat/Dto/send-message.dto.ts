5917061225eb239e24839dde58e22c4f75526408
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-12-03 11:40:47 +0700

    remove unused var

--- src/Chat/Dto/send-message.dto.ts
+++ src/Chat/Dto/send-message.dto.ts
@@ -1,10 +1,6 @@
 import { IsUUID, IsString, IsNotEmpty } from 'class-validator';
 
 export class SendMessageDto {
-  @IsUUID()
-  @IsNotEmpty()
-  cm_cr_id: string; // ID room tempat pesan dikirim
-
   @IsString()
   @IsNotEmpty()
   message_text: string; // Isi pesan
