cb7f8e80a65962da1448c9c3559ab72ea921f6ae
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-12-15 11:57:31 +0700

    remove class transformer residue

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  /* Enable global validation pipe so DTOs (class-validator) are enforced
-    - whitelist: strip properties that do not have any decorator in the DTO
-    - forbidNonWhitelisted: throw error when unknown props are present
-    - transform: automatically transform payloads to DTO instances */
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
