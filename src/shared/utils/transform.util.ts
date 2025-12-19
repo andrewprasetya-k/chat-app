a407a01cd8d7e4cbc2ab37338da7ccee056fc54b
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-12-19 14:51:54 +0700

    remove console log on transfomr.util

--- src/shared/utils/transform.util.ts
+++ src/shared/utils/transform.util.ts
@@ -1,22 +1,22 @@
-import { plainToInstance, instanceToPlain, ClassConstructor } from 'class-transformer';
+import {
+  plainToInstance,
+  instanceToPlain,
+  ClassConstructor,
+} from 'class-transformer';
 
 export class TransformUtil {
   static toEntity<T, V>(cls: ClassConstructor<T>, plain: V | V[]): T | T[] {
-    console.log('Input to toEntity:', plain);
     const result = plainToInstance(cls, plain, {
       excludeExtraneousValues: true,
       enableImplicitConversion: true,
     });
-    console.log('Result from toEntity:', result);
     return result;
   }
 
   static toPlain<T>(instance: T | T[]): any {
-    console.log('Input to toPlain:', instance);
     const result = instanceToPlain(instance, {
       excludeExtraneousValues: true,
     });
-    console.log('Result from toPlain:', result);
     return result;
   }
 
