28cefe0081c12006f79e553a6162ef0ee866e0fe
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-12-19 08:59:46 +0700

    remove byWho in dto

--- src/Chat/Dto/add-remove-member.dto.ts
+++ src/Chat/Dto/add-remove-member.dto.ts
@@ -10,15 +10,13 @@ import {
 } from 'class-validator';
 
 export class AddRemoveMemberDto {
-  @IsString()
-  @IsNotEmpty({ message: 'Chat room ID should not be empty' })
-  chatRoomId: string; // tidak boleh kosong
-
   @IsArray()
-  @ArrayMinSize(1, { message: 'Minimal harus ada 2 anggota dalam room.' })
+  @ArrayMinSize(1, {
+    message: 'Minimal harus ada 1 orang yang ingini ditambahkan ke dalam room.',
+  })
   @IsUUID('4', {
     each: true,
     message: 'Setiap user_id harus berupa UUID valid.',
   })
-  groupMembers: string[]; // daftar user_id
+  members: string[]; // daftar user_id
 }
