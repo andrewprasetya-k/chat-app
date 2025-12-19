747229a17f0715cc0b3a3038b35acc4ca8ad997e
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-12-19 13:32:08 +0700

    added async and await on chat controller

--- src/Chat/Controller/chat.controller.ts
+++ src/Chat/Controller/chat.controller.ts
@@ -18,50 +18,50 @@ export class ChatController {
 
   @Post('send/:roomId')
   @UseGuards(AuthGuard)
-  sendMessage(
+  async sendMessage(
     @Body() body: SendMessageDto,
     @Param('roomId') roomId: string,
     @User('sub') userId: string,
   ) {
-    return this.chatService.sendMessage(body, roomId, userId);
+    return await this.chatService.sendMessage(body, roomId, userId);
   }
 
   @Post('read/:roomId/:messageId')
   @UseGuards(AuthGuard)
-  markAsRead(
+  async markAsRead(
     @Param('messageId') messageId: string,
     @Param('roomId') roomId: string,
     @User('sub') userId: string,
   ) {
-    return this.chatService.markMessageAsRead(roomId, messageId, userId);
+    return await this.chatService.markMessageAsRead(roomId, messageId, userId);
   }
 
   @Get('unread-count/:roomId')
   @UseGuards(AuthGuard)
-  getUnreadCount(
+  async getUnreadCount(
     @User('sub') userId: string,
     @Param('roomId') roomId: string,
   ) {
-    return this.chatService.countUnreadMessages(roomId, userId);
+    return await this.chatService.countUnreadMessages(roomId, userId);
   }
 
   @Patch('unsend/:roomId/:messageId')
   @UseGuards(AuthGuard)
-  unsendMessage(
+  async unsendMessage(
     @Param('messageId') messageId: string,
     @Param('roomId') roomId: string,
     @User('sub') userId: string,
   ) {
-    return this.chatService.unsendMessage(roomId, messageId, userId);
+    return await this.chatService.unsendMessage(roomId, messageId, userId);
   }
 
   @Get('search/:roomId/:query')
   @UseGuards(AuthGuard)
-  searchMessages(
+  async searchMessages(
     @Param('roomId') roomId: string,
     @Param('query') query: string,
     @User('sub') userId: string,
   ) {
-    return this.chatService.searchMessages(roomId, query, userId);
+    return await this.chatService.searchMessages(roomId, query, userId);
   }
 }
