25d58dd8cb462500c21c4143242ba92008a45073
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-12-23 14:40:59 +0700

    added room active guard to module

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -3,10 +3,16 @@ import { ChatSharedService } from './chat-shared.service';
 import { SupabaseModule } from 'src/Supabase/supabase.module';
 import { RoomMemberGuard } from './guards/room-member.guard';
 import { RoomAdminGuard } from './guards/room-admin.guard';
+import { RoomActiveGuard } from './guards/room-active.guard';
 
 @Module({
   imports: [SupabaseModule],
-  providers: [ChatSharedService, RoomMemberGuard, RoomAdminGuard],
-  exports: [ChatSharedService, RoomMemberGuard, RoomAdminGuard],
+  providers: [
+    ChatSharedService,
+    RoomMemberGuard,
+    RoomAdminGuard,
+    RoomActiveGuard,
+  ],
+  exports: [ChatSharedService, RoomMemberGuard, RoomAdminGuard, RoomActiveGuard],
 })
 export class SharedModule {}
