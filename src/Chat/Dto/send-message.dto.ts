8e473d467ac1d2fcc01bf7043c5d55aa9bd01022
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-12-26 13:14:21 +0700

    added isoptional for send-message.dto

--- src/Chat/Dto/send-message.dto.ts
+++ src/Chat/Dto/send-message.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class SendMessageDto {
   @IsString()
@@ -6,5 +6,6 @@ export class SendMessageDto {
   text: string; // Isi pesan
 
   @IsString()
+  @IsOptional()
   replyTo?: string; // ID pesan yang dibalas (opsional)
 }
