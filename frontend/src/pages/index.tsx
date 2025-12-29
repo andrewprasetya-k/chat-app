ab9ab57fb96476bc1c2880b1ad5855e9b4cc14b9
andrewprasetya-k <andrewprasetya.k@gmail.com>
2025-12-29 16:12:28 +0700

    added push to login page on index

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,15 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    // Redirect to login page on initial load
+    router.push("/login");
+  }, [router]);
+
   return (
     <>
       <Head>
