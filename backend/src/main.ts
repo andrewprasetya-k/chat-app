a23db58628e0dff96a767b3931e89d725326344b
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-01-01 22:13:24 +0700

    added cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3001',
+      'https://c8sfg3vs-3001.asse.devtunnels.ms',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
