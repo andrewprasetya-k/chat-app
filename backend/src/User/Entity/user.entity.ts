b132e3a81081981a61195f974e9b588b3d151ecf
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-01-06 13:07:47 +0700

    revert edit on user entity

--- backend/src/User/Entity/user.entity.ts
+++ backend/src/User/Entity/user.entity.ts
@@ -4,15 +4,15 @@ import { BaseEntity } from '../../shared/entities/base.entity';
 export class UserEntity extends BaseEntity {
   @Expose()
   @Transform(({ obj }) => obj.usr_id, { toClassOnly: true })
-  userId: string;
+  id: string;
 
   @Expose()
   @Transform(({ obj }) => obj.usr_nama_lengkap, { toClassOnly: true })
-  userFullName: string;
+  fullName: string;
 
   @Expose()
   @Transform(({ obj }) => obj.usr_email, { toClassOnly: true })
-  userEmail: string;
+  email: string;
 
   @Expose()
   @Transform(({ obj }) => obj.usr_avatar, { toClassOnly: true })
@@ -20,7 +20,7 @@ export class UserEntity extends BaseEntity {
 
   @Expose()
   @Transform(({ obj }) => obj.usr_role, { toClassOnly: true }) // Default value
-  userRole?: string; // Optional with default
+  role?: string; // Optional with default
 
   @Exclude()
   password?: string;
