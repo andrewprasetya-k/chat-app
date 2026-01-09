0393e00b95405d944f975853efff7011f3bfe0f1
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-01-09 16:01:03 +0700

    added todo di chat widnwo

--- frontend/src/components/Chat/ChatWindow.tsx
+++ frontend/src/components/Chat/ChatWindow.tsx
@@ -317,7 +317,7 @@ export const ChatWindow: React.FC<ChatWindowProps> = ({
     if (activeRoom.isGroup) {
       return (
         <span className="text-sm text-gray-500">
-          {activeRoom.memberCount} members
+          todo: berapa member online
         </span>
       );
     } else {
